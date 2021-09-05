b4d1c85eaa0b23f693e92da1b34604f08adb68f3
codanux <codanux@hotmail.com>
2021-09-05 22:21:47 +0300

    S

---
+++ S/Address.php
@@ -0,0 +1,47 @@
+<?php
+
+class Address
+{
+    private string $country;
+    private string $city;
+
+    public function changeAddress(Address $address)
+    {
+        $this->setCountry($address->getCountry());
+        $this->setCity($address->getCity());
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param string $country
+     */
+    public function setCountry(string $country): void
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity(string $city): void
+    {
+        $this->city = $city;
+    }
+
+
+}
---
+++ S/User.php
@@ -0,0 +1,60 @@
+<?php
+
+class User
+{
+    private string $name;
+    private string $email;
+    private string $password;
+    private Address $address;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+
+
+}
